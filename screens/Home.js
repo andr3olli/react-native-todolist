ffb48a45c22e064f0ad0adb7bc5ceabf0e785ef0
andr3olli <80465306+andr3olli@users.noreply.github.com>
2021-03-11 23:48:01 +0100

    Create Home.js

---
+++ screens/Home.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import {View} from 'react-native'
+
+export default function Home() {
+  return (
+    <View>
+      <Text>Hallo mein Name ist Andreas</Text>
+    </View>
+  )
+}
