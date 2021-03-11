e99cea222e415553b955106d217b1a88b33bbb71
andr3olli <andreasilias.alexopoulos@gmail.com>
2021-03-11 23:05:21 +0100

    Screen Folder angelegt und Header bearbeitet

---
+++ components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import {Text, View} from 'react-native'
+
+export default function Header(props) {
+    return(
+        <View>
+            <Text>Hallo mein Name ist Andreas</Text>
+        </View>
+    )
+}
